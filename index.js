1775893b9951945cfb54287cd25b32c0889be4f3
mainul islam anik <mainulislamanik1710@gmail.com>
2026-05-05 03:29:03 +0600

    added property api

--- index.js
+++ index.js
@@ -39,20 +39,32 @@ async function run() {
     // USERS APIS
     app.post('/users', async (req, res)=>{
         const newUser = req.body;
-        const result = await usersCollection.insertOne(newUser);
-        res.send(result)
+        const email = req.body.email
+        const query = {email: email}
+        const existingUser = await usersCollection.findOne(query);
+        if(existingUser){
+            res.send({message: 'User already exist. Do not need to insert again'})
+        }
+        else{
+            const result = await usersCollection.insertOne(newUser);
+            res.send(result)
+        }
     })
 
-    // app.post('/register', (req, res)=>{
-    //     console.log('request',req.body)
-    //     try{
-
-    //     } catch (err) {
-
-    //     }
-    // })
+    
+    app.get('/latest-property', async (req, res) => {
+            const cursor = productsCollection.find().sort({ created_at: -1 }).limit(6);
+            const result = await cursor.toArray();
+            res.send(result);
+    })
 
 
+    app.post('/products', async (req, res) => {
+            console.log('header in the post', req.headers)
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
+        })
 
 
     await client.db("admin").command({ ping: 1 });
